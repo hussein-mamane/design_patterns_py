d118ea046732a17563a5335aca11b8f4beb1a6f8
HOUSSEINIMDB <danbouzouahousseini@gmail.com>
2023-11-11 09:14:38 +0100

    Update, added enumeration for joystick

--- functional_command.py
+++ functional_command.py
@@ -1,13 +1,23 @@
 from enum import Enum
 
 
-class ButtonEnum(Enum, str):
-    ...
+# joystick where you can press 2 buttons, enumeration
+class JoystickEnum(str, Enum):
+    BUTTON_A = 'a'
+    BUTTON_D = 'd'
+
+
+# this should be in a game loop, using the engine equivalent
+# input capture solution
+def get_pressed_button() -> str | None:
+    b = input("Press 'a' or 'd' :\n")
+    return b if b == JoystickEnum.BUTTON_A\
+        or b == JoystickEnum.BUTTON_D else None
 
 
 class Command():
     def __init__(self):
         pass
 
-    def execute(self, ButtonEnum):
+    def execute(self, JoystickEnum):
         pass
